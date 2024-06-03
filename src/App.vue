46619160c631b467c02fd6cd1a21cbe526438505
ajeevsan <sanjeevmajhi036@gmail.com>
2024-06-03 08:22:33 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <DashboardComponent/>
+    <!-- <TestComponent/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import DashboardComponent from './components/DashboardComponent.vue'
+// import TestComponent from './components/Test.vue'
+import './index.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DashboardComponent,
+    // TestComponent
   }
 }
 </script>
@@ -21,8 +26,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="w-screen h-screen bg-white flex-auto">
+        <div class=" h-10 bg-slate-600 w-screen " >
+
+        </div>
+
+        <div class="w-screen h-screen flex m-0 p-0 " >
+            <div class=" h-screen w-1/2 bg-slate-950 m-0 p-0 " ></div>
+
+            <div class=" h-screen w-1/2 bg-slate-400 m-0 p-0 " ></div>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'TestComponent',
+}
+</script>
+
+<style></style>
